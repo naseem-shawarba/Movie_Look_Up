a957af66cda70e2b55744e01d3df0e7d23c22d55
naseem-shawarba <naseem.shawarba@gmail.com>
2024-05-25 11:54:19 +0200

    changed webapp tab title to Movie Look Up

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,7 +9,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    titleTemplate: '%s - Movie_Look_up',
+    titleTemplate: 'Movie Look up',
     title: 'Movie_Look_up',
     htmlAttrs: {
       lang: 'en'
